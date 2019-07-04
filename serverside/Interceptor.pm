0b3ba8b1495d0adb4696d907c4c68e5fcca6bcf9
mig1023 <mig1023@yandex.ru>
2019-07-04 21:47:04 +0300

    proxyIP

--- serverside/Interceptor.pm
+++ serverside/Interceptor.pm
@@ -232,16 +232,13 @@ sub send_request
 	
 	return "ERR4:Не установлена кассовая интеграция" unless $serv =~ /^([0-9]{1,3}[\.]){3}[0-9]{1,3}$/;
 
-	# ////// tmp
-
-	if ( $serv eq '127.0.0.1' ) {
+	my $proxy = $vars->db->sel1("
+		SELECT ProxyIP FROM Cashboxes_interceptors WHERE ID = ?",
+		$interceptor
+	) || undef;
 	
-		$serv = '127.0.0.1';
-		$port = '80';
-	}
+	( $serv, $port ) = split( /:/, $proxy ) if $proxy;
 	
-	# //////
-
 	my $ua = LWP::UserAgent->new;
 	
 	$ua->agent( 'Mozilla/4.0 (compatible; MSIE 6.0; X11; Linux i686; en) Opera 7.60' );
